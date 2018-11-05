0204c604539379029f010e22e89a96d28f0e25b7
Pablo López <pablopez7@gmail.com>
2018-11-04 18:50:20 -0600

    Angular 7, Angular Material, MaterializeCss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as M from "../assets/materialize/js/materialize.min.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'smartSystems';
+
+  options = {};
+
+  
+  ngOnInit(){
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, this.options);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './sharedModules/material.module';
+
+//Regular Modules
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
